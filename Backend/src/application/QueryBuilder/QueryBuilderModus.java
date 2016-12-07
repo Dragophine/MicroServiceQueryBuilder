e9989649ad9105fc3e2282e6624664bd9b08f8fc
reite <reite@140.78.231.22>
2016-12-07 15:41:39 +0100

    QueryBuilder mit Rekursion

--- Backend/src/application/QueryBuilder/QueryBuilderModus.java
+++ Backend/src/application/QueryBuilder/QueryBuilderModus.java
@@ -1,23 +1,20 @@
 package application.QueryBuilder;
 
-import java.text.MessageFormat;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
 
-import org.neo4j.ogm.model.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.neo4j.template.Neo4jOperations;
 import org.springframework.data.neo4j.template.Neo4jTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import application.entity.ExpertQuery;
 import application.entity.FilterAttribute;
 import application.entity.Node;
 import application.entity.OrderByAttribute;
@@ -39,6 +36,16 @@ public class QueryBuilderModus {
 	@Autowired
 	ParameterRepository parameterRepository;
 	
+	//required global variables
+	Map<String,String> synonyms = new HashMap<String,String>();
+	Map<String,Object> paramsMap = new HashMap<String,Object>();
+	char synonym='a';
+	char end='z';
+	Set<Parameter> parameter = new HashSet<Parameter>(0);
+	String query = "";
+	LinkedList<String> filterStatements = new LinkedList<String>();
+	LinkedList<String> orderStatements = new LinkedList<String>();
+	LinkedList<String> returnStatements = new LinkedList<String>();
 	
 	
 	//TODO: rekursiv gestalten; dabei darauf achten, dass orderby, filter und returnattribute (in filterstring, returnstring, orderstring) rekursiv angepasst werden bzw. die strings erweitert werden
@@ -49,18 +56,56 @@ public class QueryBuilderModus {
 	@RequestMapping(value="/buildQuery",  method=RequestMethod.POST)
 //	public ResponseEntity<Result> buildQuery(@RequestBody QueryBuilder queryBuilder) throws Exception {
 	public String buildQuery(@RequestBody QueryBuilder queryBuilder) throws Exception {
+		filterStatements.clear();
+		orderStatements.clear();
+		returnStatements.clear();
+		synonyms.clear();
+		synonym = 'a';
+		String query = "MATCH";
+		
 		QueryBuilderStringObject queryBuilderStringObject = new QueryBuilderStringObject();
-		Map<String,String> synonyms = new HashMap<String,String>();
-		Node firstNode = queryBuilder.getNode();
-		char synonym='a';
-		char end='z';
+		
+		Node node = queryBuilder.getNode();
+		
+		query += buildNode(node);
+		
+		query += " WHERE ";
+		
+		Iterator<String> it = filterStatements.iterator();
+		while (it.hasNext()){
+			query += it.next();
+			if (it.hasNext()) query += " AND ";
+		}
+		
+		query += " RETURN ";
+		
+		it = returnStatements.iterator();
+		while (it.hasNext()){
+			query += it.next();
+			if (it.hasNext()) query += ", ";
+		}
+		
+		query += " ORDER BY ";
+		
+		it = orderStatements.iterator();
+		while (it.hasNext()){
+			query += it.next();
+			if (it.hasNext()) query += ", ";
+		}
+		
+		return query;
+		
+/*		Node firstNode = queryBuilder.getNode();
+
 		String queryString="MATCH ("+synonym+":"+firstNode.getType()+")";
+
+		
 		synonyms.put(firstNode.getType(),String.valueOf(synonym));		
 		synonym++;
-		Map<String,Object> paramsMap = new HashMap<String,Object>();
+		
 		String finalstring="";
 		
-		Set<Parameter> parameter = new HashSet<Parameter>(0);
+		
 	
 		queryString=queryString+" %s ";
 		
@@ -71,6 +116,7 @@ public class QueryBuilderModus {
 		boolean filter=false;
 		boolean order=false;
 		boolean returna=false;
+		
 		if (!firstNode.getFilterAttributes().isEmpty()){
 			filter=true;
 			queryString=queryString+" where ";
@@ -111,7 +157,6 @@ public class QueryBuilderModus {
 				if (i>1)	orderstring=orderstring+", ";
 				orderstring=orderstring+synonyms.get(firstNode.getType())+"."+o.getAttributeName()+" "+o.getDirection();
 			}
-		
 		}
 		
 		
@@ -185,27 +230,129 @@ public class QueryBuilderModus {
 					
 				}
 				
-				String teststring= String.format(queryString, relationship, filterstring);
-				finalstring=teststring+finalstring;
+//				String teststring= String.format(queryString, relationship, filterstring);
+//				finalstring=teststring+finalstring;
 				
 			}
 		} 
 	
-		
-		
+	String teststring= String.format(queryString, relationship, filterstring);
+	finalstring=teststring+finalstring;	
 		//Result result = neo4jOperations.query(queryBuilderStringObject.getQueryString(),queryBuilderStringObject.getParamsMap(), true);
 	
 
 //	return new ResponseEntity<Result>(result, HttpStatus.OK);
 	finalstring=finalstring+returnstring+orderstring;
 	return finalstring;
+	
+	*/
 	}
 	
 	
-	private QueryBuilderStringObject buildQueryRecursive(QueryBuilderStringObject qb){
-		
-		
+	private QueryBuilderStringObject buildQueryRecursive(QueryBuilderStringObject qb){	
 		return qb;
 	}
 	
+	
+	private String buildNode(Node node){
+		
+		String doublePoint = "";
+		if (node.getType() != "") doublePoint = ":";
+		
+		String nodeString = "(" + synonym + doublePoint + node.getType() + ")";
+		
+		//TODO was wenn kein Typ angegeben?
+		synonyms.put(node.getType(),String.valueOf(synonym));		
+		synonym++;
+		
+		//filter zusammenstellen
+		if (!node.getFilterAttributes().isEmpty()){
+			solveFilter(node.getFilterAttributes(), node.getType());
+		}
+		
+		//order zusammenstellen
+		if (!node.getOrderByAttributes().isEmpty()){
+			solveOrder(node.getOrderByAttributes(), node.getType());
+		}
+		
+		//return zusammenstellen
+		if (!node.getReturnAttributes().isEmpty()){
+			solveReturn(node.getReturnAttributes(), node.getType());
+		}
+		
+		if (node.getRelationship().isEmpty()){
+			return nodeString;
+		} else {
+			return nodeString + buildRelation(node.getRelationship().iterator().next());
+		}	
+	}
+	
+	
+	private String buildRelation(Relationship relation){
+		String relationship = "";
+		//TODO es gibt auch Verbindungen in beide Richtungen
+		if (relation.getDirection().equals("outgoing")){
+			relationship = "-[" + synonym + ":" + relation.getRelationshipType() + "]->";
+		} else {
+			relationship = "<-[" + synonym + ":" + relation.getRelationshipType()+ "]-";
+		}
+		synonyms.put(relation.getRelationshipType(),String.valueOf(synonym));		
+		synonym++;
+		
+		//filter zusammenstellen
+		if (!relation.getFilterAttributes().isEmpty()){
+			solveFilter(relation.getFilterAttributes(), relation.getRelationshipType());
+		}
+		
+		//order zusammenstellen
+		if (!relation.getOrderByAttributes().isEmpty()){
+			solveOrder(relation.getOrderByAttributes(), relation.getRelationshipType());
+		}
+		
+		//return zusammenstellen
+		if (!relation.getReturnAttributes().isEmpty()){
+			solveReturn(relation.getReturnAttributes(), relation.getRelationshipType());
+		}
+		
+		if (relation.getNode() == null){
+			return relationship;
+		} else {
+			return relationship + buildNode(relation.getNode());
+		}	
+	}
+	
+	private void solveFilter(Set<FilterAttribute> filterSet, String type){
+		int i = 1;
+		//TODO selbes Problem => was wenn kein Type??
+		
+		for (FilterAttribute f : filterSet){
+			
+			String paramName = type + f.getAttributeName() + i;
+			i++;
+			
+			Parameter newParam = new Parameter();
+			newParam.setChangeable(f.getChangeable());
+			newParam.setKey(paramName);
+			newParam.setValue(f.getValue());
+			newParam.setType(f.getType());
+			parameter.add(newParam);
+			paramsMap.put(paramName, f.getValue());
+			
+			filterStatements.add(synonyms.get(type) + "." + f.getAttributeName() + f.getFilterType() + "{" + paramName + "}");
+		}
+	}
+	
+	private void solveOrder(Set<OrderByAttribute> obSet, String type){
+		for (OrderByAttribute o : obSet){
+			orderStatements.add(synonyms.get(type) + "." + o.getAttributeName() + " " + o.getDirection());
+		}
+	}
+	
+	private void solveReturn (Set<ReturnAttribute> retSet, String type){
+		for (ReturnAttribute r : retSet){
+			returnStatements.add(r.getReturnType() + " " + synonyms.get(type) + "." + r.getAttributeName());
+		}
+	}
+	
 }
+
