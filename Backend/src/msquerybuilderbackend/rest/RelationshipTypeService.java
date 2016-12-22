c2075271284ec48633413c5bcdc0ba66160e97d6
Dragophine <dragophine@gmail.com>
2016-12-22 18:30:41 +0100

    invented RelationshipTypeService

---
+++ Backend/src/msquerybuilderbackend/rest/RelationshipTypeService.java
@@ -0,0 +1,39 @@
+package msquerybuilderbackend.rest;
+
+import java.util.HashMap;
+
+import org.neo4j.ogm.model.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.neo4j.template.Neo4jOperations;
+import org.springframework.data.neo4j.template.Neo4jTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RelationshipTypeService {
+	
+	@Autowired
+	Neo4jOperations neo4jOperations;
+	Neo4jTemplate temp;
+	
+	
+	
+	
+	@CrossOrigin
+	@RequestMapping(value="/relationshiptypes/{relationId}/keys", method=RequestMethod.GET)
+	public ResponseEntity<Result> getKeys(@PathVariable String relationId) throws Exception {
+		
+		String queryKeys = "MATCH ()-[n:"+relationId+"]->() UNWIND keys(n) AS key " +
+						 "WITH key ORDER BY key RETURN  COLLECT(distinct key) as Keys";
+
+		Result result = neo4jOperations.query(queryKeys, new HashMap<String, String>());
+		
+		return new ResponseEntity<Result>(result, HttpStatus.OK);
+	}
+	
+}
