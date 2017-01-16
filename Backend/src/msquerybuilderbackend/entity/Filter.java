a1f6f58c715b1d01b1d4e4f4a87647a17ce29805
Dragophine <dragophine@gmail.com>
2017-01-16 11:51:00 +0100

    changed structure of FilterAttribute due to inventing new functionalities

---
+++ Backend/src/msquerybuilderbackend/entity/Filter.java
@@ -0,0 +1,67 @@
+package msquerybuilderbackend.entity;
+
+public class Filter {
+	private long id;
+	
+	private String filterType="";
+//	private String key;
+	private String type="";
+	private String value="";
+	private Boolean changeable;
+	private Boolean isBracketOpen;
+	private Boolean isBracketClosed;
+	private String logic="";
+	
+	public Filter(){
+		
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFilterType() {
+		return filterType;
+	}
+	public void setFilterType(String filterType) {
+		this.filterType = filterType;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public Boolean getChangeable() {
+		return changeable;
+	}
+	public void setChangeable(Boolean changeable) {
+		this.changeable = changeable;
+	}
+	public Boolean getIsBracketOpen() {
+		return isBracketOpen;
+	}
+	public void setIsBracketOpen(Boolean isBracketOpen) {
+		this.isBracketOpen = isBracketOpen;
+	}
+	public Boolean getIsBracketClosed() {
+		return isBracketClosed;
+	}
+	public void setIsBracketClosed(Boolean isBracketClosed) {
+		this.isBracketClosed = isBracketClosed;
+	}
+	public String getLogic() {
+		return logic;
+	}
+	public void setLogic(String logic) {
+		this.logic = logic;
+	}
+}
--- Backend/src/msquerybuilderbackend/entity/FilterAttribute.java
+++ Backend/src/msquerybuilderbackend/entity/FilterAttribute.java
@@ -1,19 +1,12 @@
 package msquerybuilderbackend.entity;
 
-
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class FilterAttribute {
 	
 	private String attributeName="";
-	private String filterType="";
-//	private String key;
-	private String type="";
-	private String value="";
-	private Boolean changeable;
-	private Boolean isBracketOpen;
-	private Boolean isBracketClosed;
-	private String logic="";
+	private List<Filter> filters;
 	
 	public FilterAttribute(){
 		
@@ -23,71 +16,24 @@ public class FilterAttribute {
 		return this.attributeName;
 	}
 	
-	public String getFilterType(){
-		return this.filterType;
-	}
 	
-//	public String getKey(){
-//		return this.key;
-//	}
-	
-	public String getType(){
-		return this.type;
-	}
-	
-	public String getValue(){
-		return this.value;
-	}
-	
-	public Boolean getChangeable(){
-		return this.changeable;
-	}
-	
-	
-	public Boolean getIsBracketOpen() {
-		return isBracketOpen;
-	}
-
-	public void setIsBracketOpen(Boolean isBracketOpen) {
-		this.isBracketOpen = isBracketOpen;
-	}
-
-	public Boolean getIsBracketClosed() {
-		return isBracketClosed;
-	}
-
-	public void setIsBracketClosed(Boolean isBracketClosed) {
-		this.isBracketClosed = isBracketClosed;
-	}
-
-	public String getLogic() {
-		return logic;
-	}
-
-	public void setLogic(String logic) {
-		this.logic = logic;
-	}
-
 	public void setAttributeName(String at){
 		this.attributeName=at;
 	}
-	
-	public void setFilterType(String ft){
-		this.filterType=ft;
+
+	public List<Filter> getFilter() {
+		if (filters==null) return new ArrayList<Filter>();
+		return filters;
+	}
+
+	public void setFilter(List<Filter> filter) {
+		this.filters = filter;
 	}
 	
-//	public void setKey(String k){
-//		this.key=k;
-//	}
-	public void setType(String t){
-		this.type=t;
+	public void addFilter(Filter filter){
+		if (filter==null) this.filters=new ArrayList<Filter>();
+		this.filters.add(filter);
 	}
 	
-	public void setValue(String val){
-		this.value=val;
-	}
 	
-	public void setChangeable(Boolean c){
-		this.changeable=c;
-	}
 }
