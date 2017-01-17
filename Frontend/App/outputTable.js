3baa28ed1c70f661fce5e37fbbfbafe6593ca454
johann_paul_st_bich <johannstoebich@johanns-mbp>
2017-01-17 19:04:35 +0100

    Änderungen

--- Frontend/App/outputTable.js
+++ Frontend/App/outputTable.js
@@ -3,9 +3,9 @@
 angular.module('OutputTable', [])
   .controller('Controller', ['$scope', function($scope) {
 
-        $scope.input="B";
+    $scope.input="B";
 
-          $scope.setData = function (data) {
+    $scope.setData = function (data) {
         $scope.input = data;
        // console.log($scope.input);
         $scope.data = convertTableWidgetData(data);
@@ -13,121 +13,125 @@ angular.module('OutputTable', [])
     //console.log($scope.input);
 
             
-            var widgetData = $scope.input;
-            //var data = "";
-            //console.log($scope.json);
+    var widgetData = $scope.input;
+    //var data = "";
+    //console.log($scope.json);
 
         
-        function convertTableWidgetData(widgetData, div) {
-          var headerData = getTableHeader(widgetData);
-          var bodyData = getTableBody(widgetData);
-          return {tableHeader:headerData, tableBody:bodyData};
+    function convertTableWidgetData(widgetData, div) {
+      var headerData = getTableHeader(widgetData);
+      var bodyData = getTableBody(widgetData);
+      return {tableHeader:headerData, tableBody:bodyData};
+    }
+
+    function getTableHeader(data){
+      // convert header data
+      //console.log(data[0]);
+      //console.log(data[1]);
+      var headerData = convertTableHeader(data[0], 1);
+      // get max level
+      var maxLevel = 0;
+      for(var i=0;i<headerData.length;i++){
+        if(headerData[i].level>maxLevel){
+          maxLevel = headerData[i].level;
         }
-        function getTableHeader(data){
-          // convert header data
-          //console.log(data[0]);
-          //console.log(data[1]);
-          var headerData = convertTableHeader(data[0], 1);
-          // get max level
-          var maxLevel = 0;
-          for(var i=0;i<headerData.length;i++){
-            if(headerData[i].level>maxLevel){
-              maxLevel = headerData[i].level;
+      }
+      // generate headlines
+      var tableColumns = [];
+      for (var level = 1; level <= maxLevel; level++) {
+        var headerRow = [];
+        for (var i = 0; i < headerData.length; i++) {
+          if (headerData[i].level == level) {
+            var rowSpan = 1;
+            if (headerData[i].elementType == "primitive") {
+              rowSpan = maxLevel - headerData[i].level + 1;
             }
+            headerRow.push({title: headerData[i].title,colspan:headerData[i].colspan,rowspan:rowSpan});
           }
-          // generate headlines
-          var tableColumns = [];
-          for (var level = 1; level <= maxLevel; level++) {
-            var headerRow = [];
-            for (var i = 0; i < headerData.length; i++) {
-              if (headerData[i].level == level) {
-                var rowSpan = 1;
-                if (headerData[i].elementType == "primitive") {
-                  rowSpan = maxLevel - headerData[i].level + 1;
-                }
-                headerRow.push({title: headerData[i].title,colspan:headerData[i].colspan,rowspan:rowSpan});
+        }
+        tableColumns.push(headerRow);
+      }
+      return tableColumns;
+    }
+        
+    function convertTableHeader(data, level) {
+      var headerData = [];
+      for (var prop in data) {
+          //console.log(data[prop]);
+        if (typeof data[prop] == 'object' && data[prop] != null) {
+          //console.log(data[prop][0]);
+          var subHeaderData = convertTableHeader(data[prop], level + 1);                
+          var colspan = 0;
+          for(var i=0;i<subHeaderData.length;i++){
+            colspan += subHeaderData[i].colspan;
+            headerData.push(subHeaderData[i]);
+          }
+          headerData.push({ title: prop, level: level, colspan: colspan, elementType: "object"});
+        }
+        else{
+          headerData.push({ title: prop, level: level, colspan: 1, elementType: "primitive"});
+        }
+      }
+      return headerData;
+    }
+
+    function getTableBody(data){
+      var tableRows = [];
+      for(var j = 0;j < data.length; j++){
+        var bodyData = convertTableBody(data[j], 1);
+        // get max level
+        var maxLevel = 0;
+        for(var i=0;i<bodyData.length;i++){
+          if(bodyData[i].level>maxLevel){
+            maxLevel = bodyData[i].level;
+          }
+        }
+        // generate rows
+        for (var level = 1; level <= maxLevel; level++) {
+          var row = [];
+          for (var i = 0; i < bodyData.length; i++) {
+            if (bodyData[i].level == level) {
+              var rowSpan = 1;
+              if (bodyData[i].elementType == "primitive") {
+                rowSpan = maxLevel - bodyData[i].level + 1;
               }
-            }
-            tableColumns.push(headerRow);
-          }
-          return tableColumns;
-        }
-        function convertTableHeader(data, level) {
-          var headerData = [];
-          for (var prop in data) {
-              //console.log(data[prop]);
-            if (typeof data[prop] == 'object' && data[prop] != null) {
-              //console.log(data[prop][0]);
-              var subHeaderData = convertTableHeader(data[prop], level + 1);                
-              var colspan = 0;
-              for(var i=0;i<subHeaderData.length;i++){
-                colspan += subHeaderData[i].colspan;
-                headerData.push(subHeaderData[i]);
-              }
-              headerData.push({ title: prop, level: level, colspan: colspan, elementType: "object"});
-            }
-            else{
-              headerData.push({ title: prop, level: level, colspan: 1, elementType: "primitive"});
+              row.push({title: bodyData[i].title,colspan:bodyData[i].colspan,rowspan:rowSpan});
             }
           }
-          return headerData;
+          tableRows.push(row);
         }
-        function getTableBody(data){
-          var tableRows = [];
-          for(var j = 0;j < data.length; j++){
-            var bodyData = convertTableBody(data[j], 1);
-            // get max level
-            var maxLevel = 0;
-            for(var i=0;i<bodyData.length;i++){
-              if(bodyData[i].level>maxLevel){
-                maxLevel = bodyData[i].level;
-              }
+      }
+      return tableRows;
+    }
+
+    function convertTableBody(data, level) {
+      //console.log(data);
+      var bodyData = [];
+      for (var prop in data) {
+          //console.log(prop);
+        if (typeof data[prop] == "object" && data[prop] != null) {
+          var subBodyData;
+          //console.log(data[prop]);
+          //console.log(data[prop].length);
+          //for(var i=0;i<data[prop].length;i++){
+            subBodyData = convertTableBody(data[prop], level + 1);             
+            var colspan = 0;
+            for(var j=0;j<subBodyData.length;j++){
+              colspan += subBodyData[j].colspan;
+              subBodyData[j].elementType = "object";
+              bodyData.push(subBodyData[j]);
             }
-            // generate rows
-            for (var level = 1; level <= maxLevel; level++) {
-              var row = [];
-              for (var i = 0; i < bodyData.length; i++) {
-                if (bodyData[i].level == level) {
-                  var rowSpan = 1;
-                  if (bodyData[i].elementType == "primitive") {
-                    rowSpan = maxLevel - bodyData[i].level + 1;
-                  }
-                  row.push({title: bodyData[i].title,colspan:bodyData[i].colspan,rowspan:rowSpan});
-                }
-              }
-              tableRows.push(row);
-            }
-          }
-          return tableRows;
+          //}   
         }
-        function convertTableBody(data, level) {
-          //console.log(data);
-          var bodyData = [];
-          for (var prop in data) {
-              //console.log(prop);
-            if (typeof data[prop] == "object" && data[prop] != null) {
-              var subBodyData;
-              //console.log(data[prop]);
-              //console.log(data[prop].length);
-              //for(var i=0;i<data[prop].length;i++){
-                subBodyData = convertTableBody(data[prop], level + 1);             
-                var colspan = 0;
-                for(var j=0;j<subBodyData.length;j++){
-                  colspan += subBodyData[j].colspan;
-                  subBodyData[j].elementType = "object";
-                  bodyData.push(subBodyData[j]);
-                }
-              //}   
-            }
-            else{
-              bodyData.push({ title: data[prop], level: level, colspan: 1, elementType: "primitive"});
-            }
-          }
-          //console.log(bodyData);
-          return bodyData;
+        else{
+          bodyData.push({ title: data[prop], level: level, colspan: 1, elementType: "primitive"});
         }
-      }]
-  )
+      }
+      //console.log(bodyData);
+      return bodyData;
+    }
+  }]
+)
 
 
  .directive('outputtable', function() {
@@ -143,10 +147,10 @@ angular.module('OutputTable', [])
               var thisscope = scope;
 
               scope.$watch('table', function(newvalue, oldvalue) {
-                 if(newvalue){
+                // if(newvalue !== oldvalue){ //everythime--> otherwise there will be an error in the query builder
                    //console.log(newvalue);
-                    thisscope.setData(newvalue);
-                 }
+                    thisscope.$parent.setData(newvalue);
+                //}
 
               }, true);
 
